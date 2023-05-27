3772d632de672aa60ebdead28c9bd89b6ea389f9
robss31 <robss31@hotmail.com>
2023-05-27 18:37:12 -0300

    Atualização dos arquivos

--- roteiro.js
+++ roteiro.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector("#send");
 
-
 btn.addEventListener("click", function (e) {
 
     e.preventDefault();
@@ -11,12 +10,45 @@ btn.addEventListener("click", function (e) {
 
     const converte = valor / 3.6 ;
 
-    //const converte1 = valor  * 3.6;
+    document.getElementById("recebe").innerHTML= (converte.toFixed(2) + " m/s ");
 
-    document.getElementById("valor").innerHTML= (converte.toFixed(2) + " m/s ");
-    //document.getElementById("valo").innerHTML= (converte1.toFixed(2) + " km/h ");
-
-    console.log(converte);
-    //console.log(converte1);
-    console.log((typeof(converte)))
 })
+
+const btn2 = document.querySelector("#send");
+
+btn2.addEventListener("click", function (e) {
+
+    e.preventDefau
+
+    const name = document.querySelector("#name")
+
+    const valor = Number(name.value);
+
+    const converte1 = valor  * 3.6;
+
+    document.getElementById("recebe1").innerHTML= (converte1.toFixed(2) + " km/h ");
+
+})
+
+function principal(){
+    window.location.replace("http://127.0.0.1:5500/index.html")
+}
+
+
+function  km() {
+    window.location.replace("http://127.0.0.1:5500/km.html")
+}
+
+function m() {
+    window.location.replace("http://127.0.0.1:5500/ms.html")
+}
+
+function limpar(){
+    document.getElementById("name").value = "";
+    document.getElementById("recebe").innerHTML = "";
+}
+
+function limpar1(){
+    document.getElementById("name").value = "";
+    document.getElementById("recebe1").innerHTML = "";
+}
